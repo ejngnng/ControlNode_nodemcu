5dd434dfb4327799be1e431d513c0d8048669e0f
ejngnng <ejngnng@163.com>
2018-03-07 10:56:00 +0800

    add common file for debug switch

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
+#include "common/common.h"
+
 WiFiClient espClient;
 PubSubClient MQTTClient(espClient);
 
@@ -10,6 +12,7 @@ StaticJsonBuffer<200> jsonBuffer;
 
 void setup() {
     // put your setup code here, to run once:
+    serial_init();
 }
 
 void loop() {
