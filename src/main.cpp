59069a76a5a38f903e8060c727367bb039a3fa41
ejngnng <ejngnng@163.com>
2018-03-07 10:38:09 +0800

    add ArduinoJson lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+WiFiClient espClient;
+PubSubClient MQTTClient(espClient);
+
+StaticJsonBuffer<200> jsonBuffer;
 
 void setup() {
     // put your setup code here, to run once:
